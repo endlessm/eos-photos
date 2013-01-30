ba9e14d0b0442eb515a1948147ae1da0e2469198
Rory MacQueen <rory@rory-VirtualBox.(none)>
2013-01-30 11:57:52 -0800

    add open file functionality

--- src/photos_presenter.py
+++ src/photos_presenter.py
@@ -1,3 +1,8 @@
+import os
+from gi.repository import Gtk, GdkPixbuf, Gdk
+import Image
+import StringIO
+
 class PhotosPresenter(object):
     def __init__(self, model=None, view=None):
         self._model = model
@@ -13,6 +18,56 @@ class PhotosPresenter(object):
 
     def open(self):
         print "Open called"
+        TEMP_FILE = os.path.expanduser("~/Desktop/temp.jpg")
+        
+        # Opens a dialog window where the user can choose an image file
+        dialog = Gtk.FileChooserDialog ("Open Image", None, Gtk.FileChooserAction.OPEN);
+
+        # Adds 'Cancel' and 'Open' buttons
+        dialog.add_button (Gtk.STOCK_CANCEL, 0)
+        dialog.add_button (Gtk.STOCK_OK, 1)
+
+        # Sets deafault to 'Open'
+        dialog.set_default_response(1)
+
+        # Filters and displays files which can be opened by Gtk.Image
+        filefilter = Gtk.FileFilter ()
+        filefilter.add_pixbuf_formats ()
+        dialog.set_filter(filefilter)
+
+        if dialog.run() == 1:
+
+            # Loads the image
+            FILE_NAME = dialog.get_filename()
+            new_image = Gtk.Image(file=FILE_NAME, name="photo-image")
+            #im = Image.open(FILE_NAME)
+
+            # Creates a thumbnail of the given size
+            # size = 50, 50
+            # im.thumbnail(size, Image.ANTIALIAS)
+            #im.save(TEMP_FILE)
+
+            # Converts image to pixbuf
+            #pbuf = self.image_to_pixbuf(im)
+            self._view.replace_image(new_image)
+            # Displays the chosen image
+            #self.image.set_from_pixbuf(pbuf)
+
+        dialog.destroy()
+
+
+    def image_to_pixbuf(self, img):
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
+        buff = StringIO.StringIO()
+        img.save(buff, 'ppm')
+        contents = buff.getvalue()
+        buff.close()
+        loader = GdkPixbuf.PixbufLoader.new_with_type('pnm')
+        loader.write(contents)
+        pixbuf = loader.get_pixbuf()
+        loader.close()
+        return pixbuf
 
     def save(self):
         
