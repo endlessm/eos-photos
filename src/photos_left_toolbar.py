f36eb58952c3e9b95ca39c8f47acdb01b08b8694
Rory MacQueen <rory@rory-VirtualBox.(none)>
2013-03-06 19:09:07 -0800

    Added multiple categories with expander

--- src/photos_left_toolbar.py
+++ src/photos_left_toolbar.py
@@ -1,5 +1,6 @@
 from gi.repository import Gtk
 from widgets.list_button import ListButton
+import random
 
 class PhotosLeftToolbar(Gtk.VBox):
     """
@@ -11,23 +12,17 @@ class PhotosLeftToolbar(Gtk.VBox):
 
         self._categories = {}
 
-        self._borders_image = Gtk.Image.new_from_file(images_path + "Filter-icon.png")
-        self._borders_label = Gtk.Label(label="BORDERS", name="borders-title")
-        self._borders_title_box = Gtk.HBox(homogeneous=False, spacing=0)
-        self._borders_title_box.pack_start(self._borders_image, expand=False, fill=False, padding=0)
-        self._borders_title_box.pack_start(self._borders_label, expand=False, fill=False, padding=2)
-        self._borders_title_allign = Gtk.HBox(homogeneous=False, spacing=0)
-        self._borders_title_allign.pack_start(self._borders_title_box, expand=False, fill=False, padding=10)
-
-
-        borders_category = Category(images_path=self._images_path,label_name="BORDERS", category_name="borders",
+        self._categories["borders"] = Category(images_path=self._images_path,label_name="BORDERS", category_name="borders",
             clicked_callback=lambda: self.change_category("borders"))
 
-        filters_category = Category(images_path=self._images_path,label_name="FILTROS", category_name="filters",
+        self._categories["filters"] = Category(images_path=self._images_path,label_name="FILTROS", category_name="filters",
             clicked_callback=lambda: self.change_category("filters"))
 
-        self._categories["filters"] = filters_category
-        self._categories["borders"] = borders_category
+        self._categories["text"] = Category(images_path=self._images_path,label_name="TEXTO", category_name="text",
+            clicked_callback=lambda: self.change_category("text"))
+
+        self._categories["transforms"] = Category(images_path=self._images_path,label_name="TRANSFORMS", category_name="transforms",
+            clicked_callback=lambda: self.change_category("transforms"))        
 
 
         #self._scroll_contents = Gtk.VBox(homogeneous=False, spacing=8)
@@ -37,27 +32,33 @@ class PhotosLeftToolbar(Gtk.VBox):
         # self._scroll_area.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
         # self._scroll_area.add_with_viewport(self._scroll_contents)
 
-        # self._drop_shadow = Gtk.Image.new_from_file(images_path + "Filter-mask-shadow.png")
-        # self._drop_shadow.set_halign(Gtk.Align.CENTER)
-        # self._drop_shadow.set_valign(Gtk.Align.START)
+
         # self._overlay = Gtk.Overlay()
         # self._overlay.add(self._scroll_area)
         # self._overlay.add_overlay(self._drop_shadow)
 
         #self.pack_start(self._borders_title_allign, expand=False, fill=False, padding=20)
-        self.pack_start(filters_category, expand=True, fill=True, padding=20)
-        self.pack_start(borders_category, expand=True, fill=True, padding=20)
+        for category in self._categories.values():
+            self.pack_start(category, expand=False, fill=False, padding=20)
+            drop_shadow = Gtk.Image.new_from_file(images_path + "Filter-mask-shadow.png")
+            drop_shadow.set_halign(Gtk.Align.CENTER)
+            drop_shadow.set_valign(Gtk.Align.START)
+            #self.pack_start(drop_shadow, expand=False, fill=False, padding=0)
 
         #self.pack_start(self._overlay, expand=True, fill=True, padding=0)
 
         self.show_all()
+
+        for category in self._categories.values():
+            category.expander.hide()
 
     def set_filter_names(self, filter_names, default):
         map(self._add_filter_option, filter_names)
         self._filter_options[default].select()
 
     def _add_filter_option(self, filter_name):
-        category = self._categories["filters"]
+         
+        category = self._categories[random.choice(self._categories.keys())]
         
         path = "filter_thumbnails/filter_" + filter_name + ".jpg"
         option = ListButton(images_path = self._images_path, path=path, name="filter", label_name=filter_name,
@@ -78,13 +79,13 @@ class PhotosLeftToolbar(Gtk.VBox):
 
     def change_category(self, category_name):
         for name, category in self._categories.items():
-            if name == category_name:
+            if name == category_name and not category.is_selected():
+                # TODO: Change this hard-coded 500 value to something sensible
+                category.set_size_request(-1, 500)
                 category.select()
-                category.expander.set_expanded(True)
-                #category.expanded_cb(category.expander, None)
             else:
+                category.set_size_request(-1, -1)
                 category.deselect()
-                category.expander.set_expanded(False)
 
 
 class Category(Gtk.VBox):
@@ -114,31 +115,38 @@ class Category(Gtk.VBox):
         # self._overlay.add(self._scroll_area)
         # self._overlay.add_overlay(self._drop_shadow)
 
-        self.pack_start(self.title_box, expand=False, fill=False, padding=20)
-        self.pack_start(self.expander, expand=True, fill=True, padding=20)
+        self.pack_start(self.title_box, expand=False, fill=False, padding=0)
+        self.pack_start(self.expander, expand=True, fill=True, padding=0)
         #self.pack_start(self._scroll_area, expand=True, fill=True, padding=20)
-
-        self.show_all()
+        #self.expander.hide()
+        #self.title_box.show()
+       
+        
+        self._is_selected = False
 
     def expanded_cb(self, expander, params):
-        if expander.get_expanded():
+        if expander.get_expanded():            
             expander.add(self._scroll_area)
-            self._scroll_area.show()
         else:
             expander.remove(self._scroll_area)
         self.show_all()
 
     def select(self):
+        self.expander.set_expanded(True)
+        self._is_selected = True
         self.title_box.select()
-        self._scroll_area.show()
 
     def deselect(self):
+        self.expander.set_expanded(False)
+        self._is_selected = False
         self.title_box.deselect()
-        self._scroll_area.hide()
+        self.expander.hide()
 
+    def is_selected(self):
+        return self._is_selected
 
     def add_to_scroll(self, object_):
         align = Gtk.HBox(homogeneous=False, spacing=0)
         align.pack_start(object_, expand=False, fill=False, padding=30)
         self._scroll_contents.pack_start(align, expand=False, fill=False, padding=0)
-        self.show_all()
+        #self.show_all()
