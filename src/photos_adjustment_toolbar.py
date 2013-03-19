4be8e59b235b13a848f22f91d6a51e5767678fbc
Matt Watson <mattdangerw@gmail.com>
2013-03-18 18:06:20 -0700

    Fixed bug with saturation adjustment capped at 99%.

--- src/photos_adjustment_toolbar.py
+++ src/photos_adjustment_toolbar.py
@@ -10,7 +10,7 @@ class PhotosAdjustmentToolbar(Gtk.VBox):
 
         self._brightness_label = Gtk.Label(name="filter-label", label="Brightness")
         self.pack_start(self._brightness_label, False, False, 0)
-        adj1 = Gtk.Adjustment(1.0, 0.0, 2.0, 0.01, 0.01, 0.01)
+        adj1 = Gtk.Adjustment(1.0, 0.0, 2.0, 0.01, 0, 0)
         self._brightness_slider = Gtk.HScale(adjustment=adj1, draw_value=False)
         self._brightness_slider.connect(
             "value-changed", lambda adjust: self._presenter.on_brightness_change(adjust.get_value()))
@@ -18,7 +18,7 @@ class PhotosAdjustmentToolbar(Gtk.VBox):
 
         self._contrast_label = Gtk.Label(name="filter-label", label="Contrast")
         self.pack_start(self._contrast_label, False, False, 0)
-        adj1 = Gtk.Adjustment(1.0, 0.0, 2.0, 0.01, 0.01, 0.01)
+        adj1 = Gtk.Adjustment(1.0, 0.0, 2.0, 0.01, 0, 0)
         self._contrast_slider = Gtk.HScale(adjustment=adj1, draw_value=False)
         self._contrast_slider.connect(
             "value-changed", lambda adjust: self._presenter.on_contrast_change(adjust.get_value()))
@@ -26,7 +26,7 @@ class PhotosAdjustmentToolbar(Gtk.VBox):
 
         self._saturation_label = Gtk.Label(name="filter-label", label="Saturation")
         self.pack_start(self._saturation_label, False, False, 0)
-        adj1 = Gtk.Adjustment(1.0, 0.0, 1.0, 0.01, 0.01, 0.01)
+        adj1 = Gtk.Adjustment(1.0, 0.0, 1.0, 0.01, 0, 0)
         self._saturation_slider = Gtk.HScale(adjustment=adj1, draw_value=False)
         self._saturation_slider.connect(
             "value-changed", lambda adjust: self._presenter.on_saturation_change(adjust.get_value()))
