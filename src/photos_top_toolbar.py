938eee1c9b7759bd2b6b809ab1c6a699fd4443e5
Matt Watson <mattdangerw@gmail.com>
2013-03-29 02:01:01 -0700

    Styled filters to match design deck.

--- src/photos_top_toolbar.py
+++ src/photos_top_toolbar.py
@@ -22,4 +22,4 @@ class PhotosTopToolbar(TopToolbar):
         self._left_side = Gtk.Alignment(top_padding=0, bottom_padding=0)
         self._left_side.add(self._open_button)
 
-        self.get_toolbar_container().pack_start(self._left_side, expand=False, fill=False, padding=7)
+        self.get_toolbar_container().pack_start(self._left_side, expand=False, fill=False, padding=0)
