e4591333085d675e746ebc1e78feb4f8161f63fc
Matt Watson <mattdangerw@gmail.com>
2013-09-25 14:07:01 -0700

    Small images don't scale up to fill the screen
    They stay at their natural size. Did a bit of a restructure with
    how the clutter embedding works which should make things a bit simpler
    and hopefully fix that persitent bug on VMs when loading images of
    different aspect ratios

--- src/photos_image_container.py
+++ src/photos_image_container.py
@@ -3,59 +3,63 @@ from gi.repository import Gtk, GdkPixbuf, GtkClutter, Clutter
 from widgets.clutter_image_button import ClutterImageButton
 
 
-class ImageContainer(Gtk.AspectFrame):
+class ImageContainer(GtkClutter.Embed):
     """
-    Sizes the image to fit centered in the space allotted. Expands to take up
-    any available space. Image embedded with Clutter.
+    Embeds the clutter image and sizes the image to fit centered in the space allotted.
     """
     def __init__(self, images_path="", **kw):
-        super(ImageContainer, self).__init__(obey_child=False, ratio=1.0, **kw)
-        self._embed = GtkClutter.Embed.new()
-        self._stage = self._embed.get_stage()
+        kw.setdefault("use-layout-size", True)
+        super(ImageContainer, self).__init__(**kw)
+        self._stage = self.get_stage()
+        self._stage.set_layout_manager(Clutter.BinLayout())
+        self._image_widget = None
 
-        self._fullscreen_button = ClutterImageButton(
-            normal_path=images_path + "expand-image_normal.png",
-            hover_path=images_path + "expand-image_hover.png",
-            down_path=images_path + "expand-image_down.png",
-            name="fullscreen-button",
-            depth=1.0)  # Always draw over the attached image widget
-        self._fullscreen_button.add_constraint(Clutter.AlignConstraint(
-            align_axis=Clutter.AlignAxis.BOTH, factor=1.0, source=self._stage))
-        self._fullscreen_button.get_click_action().connect('clicked', lambda w, e: self._presenter.on_fullscreen())
+        # self._fullscreen_button = ClutterImageButton(
+        #     normal_path=images_path + "expand-image_normal.png",
+        #     hover_path=images_path + "expand-image_hover.png",
+        #     down_path=images_path + "expand-image_down.png",
+        #     name="fullscreen-button",
+        #     depth=1.0)  # Always draw over the attached image widget
+        # self._fullscreen_button.add_constraint(Clutter.AlignConstraint(
+        #     align_axis=Clutter.AlignAxis.BOTH, factor=1.0, source=self._stage))
+        # self._fullscreen_button.get_click_action().connect('clicked', lambda w, e: self._presenter.on_fullscreen())
+        # self._unfullscreen_button = ClutterImageButton(
+        #     normal_path=images_path + "expand-image-close_normal.png",
+        #     hover_path=images_path + "expand-image-close_hover.png",
+        #     down_path=images_path + "expand-image-close_down.png",
+        #     name="fullscreen-button",
+        #     depth=1.0)  # Always draw over the attached image widget
+        # self._unfullscreen_button.add_constraint(Clutter.AlignConstraint(
+        #     align_axis=Clutter.AlignAxis.BOTH, factor=1.0, source=self._stage))
+        # self._unfullscreen_button.get_click_action().connect('clicked', lambda w, e: self._presenter.on_unfullscreen())
+        # self.set_fullscreen_mode(False)
 
-        self._unfullscreen_button = ClutterImageButton(
-            normal_path=images_path + "expand-image-close_normal.png",
-            hover_path=images_path + "expand-image-close_hover.png",
-            down_path=images_path + "expand-image-close_down.png",
-            name="fullscreen-button",
-            depth=1.0)  # Always draw over the attached image widget
-        self._unfullscreen_button.add_constraint(Clutter.AlignConstraint(
-            align_axis=Clutter.AlignAxis.BOTH, factor=1.0, source=self._stage))
-        self._unfullscreen_button.get_click_action().connect('clicked', lambda w, e: self._presenter.on_unfullscreen())
-
-        self.set_fullscreen_mode(False)
-
-        self.set_hexpand(True)
-        self.set_vexpand(True)
-        self.add(self._embed)
+    # We need to make sure this allocation is the right aspect ratio for our
+    # image, or there will be an obnoxiously oversized stage with a shadow
+    # even if our image is sized correctly within the stage. There's no way to
+    # quite do this properly with a size request as we don't know weather we
+    # need with for height or height for width till we get the allocation. So
+    # we do this directly in the size_allocate method
+    def do_size_allocate(self, alloc):
+        im_ratio = self._image_widget.get_ratio()
+        if self._image_widget != None:
+            alloc_ratio = float(alloc.width) / alloc.height
+            if im_ratio < alloc_ratio:
+                old_width = alloc.width
+                alloc.width = alloc.height * im_ratio
+                alloc.x += (old_width - alloc.width) / 2
+            else:
+                old_height = alloc.height
+                alloc.height = alloc.width / im_ratio
+                alloc.y += (old_height - alloc.height) / 2
+        GtkClutter.Embed.do_size_allocate(self, alloc)
 
     def set_image_widget(self, image_widget):
         self._stage.add_child(image_widget)
-        image_widget.add_constraint(Clutter.BindConstraint(
-            coordinate=Clutter.BindCoordinate.SIZE, source=self._stage))
-        image_widget.connect("notify::ratio", self._ratio_changed)
         self._image_widget = image_widget
-
-    def _ratio_changed(self, widget, property):
-        self.set_property("ratio", self._image_widget.get_property("ratio"))
-        # TODO: we are using the ratio changed signal to detect when to show
-        # the clutter stage for the first time. And that's hella hacky
 
     def set_fullscreen_mode(self, fullscreen):
         self._fullscreen_mode = fullscreen
-        # We may bring the fullscreen back someday so for now I'm removing the
-        # code that adds the fullscreen button to the scene
-
         # if fullscreen:
         #     self._stage.add_child(self._unfullscreen_button)
         #     if self._fullscreen_button.get_parent() == self._stage:
--- src/photos_image_widget.py
+++ src/photos_image_widget.py
@@ -2,16 +2,6 @@ from gi.repository import Clutter, GLib, Gdk, GObject
 
 
 class PhotosImageWidget(Clutter.Actor):
-    __gproperties__ = {
-        "ratio": (GObject.TYPE_FLOAT,
-                  "ratio",
-                  "aspect ratio of image",
-                  0.001,
-                  1000.0,
-                  1.0,
-                  GObject.PARAM_READWRITE)
-    }
-
     """
     A clutter actor which knows how to display the current model. Composites
     multiple layers of model together with Clutter. Actually a member of the
@@ -20,30 +10,17 @@ class PhotosImageWidget(Clutter.Actor):
     presenter.
     """
     def __init__(self, images_path="", **kw):
+        kw.setdefault("layout-manager", Clutter.BinLayout())
         super(PhotosImageWidget, self).__init__(**kw)
         self._ratio = 1.0
 
         self._base_image = Clutter.Texture()
-        self._base_image.add_constraint(Clutter.BindConstraint(
-            coordinate=Clutter.BindCoordinate.SIZE, source=self))
         self.add_child(self._base_image)
-
         self._border_image = Clutter.Texture()
-        self._border_image.add_constraint(Clutter.BindConstraint(
-            coordinate=Clutter.BindCoordinate.SIZE, source=self))
         self.add_child(self._border_image)
 
-    def do_get_property(self, property):
-        if property.name == "ratio":
-            return self._ratio
-        else:
-            return Clutter.Actor.do_get_property(self, property)
-
-    def do_set_property(self, property, value):
-        if property.name == "ratio":
-            self._ratio = value
-        else:
-            return Clutter.Actor.do_set_property(self, property, value)
+    def get_ratio(self):
+        return self._ratio
 
     def replace_base_image(self, data, width, height):
         Gdk.threads_add_idle(
@@ -53,7 +30,7 @@ class PhotosImageWidget(Clutter.Actor):
 
     def _replace_base_image_callback(self, data, width, height):
         self._base_image.set_from_rgb_data(data, False, width, height, 0, 3, 0)
-        self.set_property('ratio', float(width)/height)
+        self._ratio = float(width) / height
 
     def replace_border_image(self, data, width, height):
         Gdk.threads_add_idle(
