a68c071dc864a73ab4a6dd5d0add673312e07521
Philip Chimento <philip@endlessm.com>
2013-09-26 12:32:14 -0700

    Code review fixes

--- src/photos_image_container.py
+++ src/photos_image_container.py
@@ -1,6 +1,4 @@
-from gi.repository import Gtk, GdkPixbuf, GtkClutter, Clutter
-
-from widgets.clutter_image_button import ClutterImageButton
+from gi.repository import GtkClutter, Clutter
 
 
 class ImageContainer(GtkClutter.Embed):
@@ -14,31 +12,11 @@ class ImageContainer(GtkClutter.Embed):
         self._stage.set_layout_manager(Clutter.BinLayout())
         self._image_widget = None
 
-        # self._fullscreen_button = ClutterImageButton(
-        #     normal_path=images_path + "expand-image_normal.png",
-        #     hover_path=images_path + "expand-image_hover.png",
-        #     down_path=images_path + "expand-image_down.png",
-        #     name="fullscreen-button",
-        #     depth=1.0)  # Always draw over the attached image widget
-        # self._fullscreen_button.add_constraint(Clutter.AlignConstraint(
-        #     align_axis=Clutter.AlignAxis.BOTH, factor=1.0, source=self._stage))
-        # self._fullscreen_button.get_click_action().connect('clicked', lambda w, e: self._presenter.on_fullscreen())
-        # self._unfullscreen_button = ClutterImageButton(
-        #     normal_path=images_path + "expand-image-close_normal.png",
-        #     hover_path=images_path + "expand-image-close_hover.png",
-        #     down_path=images_path + "expand-image-close_down.png",
-        #     name="fullscreen-button",
-        #     depth=1.0)  # Always draw over the attached image widget
-        # self._unfullscreen_button.add_constraint(Clutter.AlignConstraint(
-        #     align_axis=Clutter.AlignAxis.BOTH, factor=1.0, source=self._stage))
-        # self._unfullscreen_button.get_click_action().connect('clicked', lambda w, e: self._presenter.on_unfullscreen())
-        # self.set_fullscreen_mode(False)
-
     # We need to make sure this allocation is the right aspect ratio for our
     # image, or there will be an obnoxiously oversized stage with a shadow
     # even if our image is sized correctly within the stage. There's no way to
-    # quite do this properly with a size request as we don't know weather we
-    # need with for height or height for width till we get the allocation. So
+    # quite do this properly with a size request as we don't know whether we
+    # need width for height or height for width till we get the allocation. So
     # we do this directly in the size_allocate method
     def do_size_allocate(self, alloc):
         im_ratio = self._image_widget.get_ratio()
