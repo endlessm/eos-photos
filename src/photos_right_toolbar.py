99aaefdea45dd1ce587bbf8db5461638c8353cf0
Martin Abente Lahaye <martin@endlessm.com>
2016-08-11 17:32:43 -0400

    Allow to disable share to facebook button
    As explained in the issue, we need a way to disable the
    share to facebook button, therefore:
    
    a) add a gsettings schema with a "share" key, so we can set
       whether we display the button or not.
    
    b) create/add the button if enabled in the key (enabled by
       default to preserve the existing behavior).
    
    https://phabricator.endlessm.com/T12869

--- src/photos_right_toolbar.py
+++ src/photos_right_toolbar.py
@@ -1,4 +1,4 @@
-from gi.repository import Endless, Gtk
+from gi.repository import Endless, Gtk, Gio
 
 from widgets.image_text_button import ImageTextButton
 
@@ -7,6 +7,7 @@ class PhotosRightToolbar(Gtk.VBox):
     """
     The right toolbar with post to facebook and save buttons.
     """
+
     def __init__(self, **kw):
         super(PhotosRightToolbar, self).__init__(homogeneous=False, vexpand=True, **kw)
 
@@ -35,16 +36,6 @@ class PhotosRightToolbar(Gtk.VBox):
             vertical=True)
         self._save_button.connect('clicked', lambda w: self._presenter.on_save())
 
-        self._share_button = ImageTextButton(
-            image_size_x=ImageTextButton.SIZE_MEDIUM,
-            image_size_y=ImageTextButton.SIZE_MEDIUM,
-            halign=Gtk.Align.CENTER,
-            margin_bottom=15,
-            label=_("FACEBOOK"),
-            name="share-button",
-            vertical=True)
-        self._share_button.connect('clicked', lambda w: self._presenter.on_share())
-
         self._bg_button = ImageTextButton(
             image_size_x=ImageTextButton.SIZE_MEDIUM,
             image_size_y=ImageTextButton.SIZE_MEDIUM,
@@ -68,12 +59,28 @@ class PhotosRightToolbar(Gtk.VBox):
         inner_button_box = Gtk.VBox(homogeneous=False, spacing=0)
         self.pack_start(self._open_photo_button, expand=False, fill=False, padding=0)
         inner_button_box.pack_start(self._save_button, expand=False, fill=False, padding=0)
-        inner_button_box.pack_start(self._share_button, expand=False, fill=False, padding=0)
+
+        if self._display_share_button():
+            self._share_button = ImageTextButton(
+                image_size_x=ImageTextButton.SIZE_MEDIUM,
+                image_size_y=ImageTextButton.SIZE_MEDIUM,
+                halign=Gtk.Align.CENTER,
+                margin_bottom=15,
+                label=_("FACEBOOK"),
+                name="share-button",
+                vertical=True)
+            self._share_button.connect('clicked', lambda w: self._presenter.on_share())
+            inner_button_box.pack_start(self._share_button, expand=False, fill=False, padding=0)
+
         inner_button_box.pack_start(self._bg_button, expand=False, fill=False, padding=0)
         self.pack_start(inner_button_box, expand=True, fill=False, padding=0)
         self.pack_end(self._revert_image_button, expand=False, fill=False, padding=0)
 
         self.set_vexpand(True)
+
+    def _display_share_button(self):
+        settings = Gio.Settings('com.endlessm.photos')
+        return settings.get_boolean('share')
 
     def set_presenter(self, presenter):
         self._presenter = presenter
