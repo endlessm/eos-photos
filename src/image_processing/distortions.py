581c19463376133196fdea339c9fecacbc2d585c
Philip Chimento <philip@endlessm.com>
2017-04-12 12:20:45 -0700

    Revert "Silence a deprecation warning on Fedora 24"
    This reverts commit 9ce7126f0c88f6c8b6f4ef62924295e0383fc517.
    
    The new code isn't compatible with the versions of SciPy and skimage that
    we are currently using to build eos-photos. Once we upgrade those, we can
    reinstate this commit.
    
    https://phabricator.endlessm.com/T16575

--- src/image_processing/distortions.py
+++ src/image_processing/distortions.py
@@ -58,27 +58,27 @@ class Distortion:
         if distort_name == "SWIRL":
             radius = max(shape[0], shape[1]) / 2
             result = swirl(self._image_array, center=(shape[1]/2, shape[0]/2), strength=5, 
-                radius=radius, order=1, mode='edge')
+                radius=radius, order=1, mode='nearest')
         elif distort_name == "FISH EYE LIGHT":
             warp_args['radius_func'] = lambda radius_d: self.fish_eye(0.3, radius_d)
             warp_args['zoom_factor'] = 1.6
-            result = warp(self._image_array, self.radius_distort, map_args=warp_args, mode='edge')
+            result = warp(self._image_array, self.radius_distort, map_args=warp_args, mode='nearest')
         elif distort_name == "FISH EYE HEAVY":
             warp_args['radius_func'] = lambda radius_d: self.fish_eye(0.6, radius_d)
             warp_args['zoom_factor'] = 2.2
-            result = warp(self._image_array, self.radius_distort, map_args=warp_args, mode='edge')
+            result = warp(self._image_array, self.radius_distort, map_args=warp_args, mode='nearest')
         elif distort_name == "BULGE":
             warp_args['radius_func'] = lambda radius_d: self.bulge(radius_d)
             warp_args['zoom_factor'] = 1.45
-            result = warp(self._image_array, self.radius_distort, map_args=warp_args, mode='edge')
+            result = warp(self._image_array, self.radius_distort, map_args=warp_args, mode='nearest')
         elif distort_name == "PINCH LIGHT":
             warp_args['radius_func'] = lambda radius_d: self.pinch(0.8, radius_d)
             warp_args['zoom_factor'] = 1
-            result = warp(self._image_array, self.radius_distort, map_args=warp_args, mode='edge')
+            result = warp(self._image_array, self.radius_distort, map_args=warp_args, mode='nearest')
         elif distort_name == "PINCH HEAVY":
             warp_args['radius_func'] = lambda radius_d: self.pinch(0.5, radius_d)
             warp_args['zoom_factor'] = 1
-            result = warp(self._image_array, self.radius_distort, map_args=warp_args, mode='edge')
+            result = warp(self._image_array, self.radius_distort, map_args=warp_args, mode='nearest')
         else:
             result = self._image_array
 
